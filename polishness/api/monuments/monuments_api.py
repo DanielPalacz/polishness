af3d7f7817d982c9b28ac593a91e013969e5bee9
danielp <you@example.com>
2024-03-28 15:19:20 +0100

    fixes bug with incorrect displaying address (None was visible when lack of info about address number)

--- polishness/api/monuments/monuments_api.py
+++ polishness/api/monuments/monuments_api.py
@@ -1,5 +1,6 @@
 """ (c) YYYY Copyright WHO """
 from sqlite3 import OperationalError
+from typing import Union
 
 from flask import Blueprint
 from flask import render_template
@@ -30,11 +31,9 @@ def wyszukaj():
     except OperationalError:
         return "Problem po stronie aplikacji. Właśnie powiadomiono administratora o zdarzeniu.", 200
 
-    #
-    print(params)
-    print("wyszukaj ... .. .")
+    _items = [tuple([elem if elem is not None else "" for elem in t_elem]) for t_elem in items]
 
-    return render_template("search.html", city=city, params=params, items=items, quantity=len(items))
+    return render_template("search.html", city=city, params=params, items=_items, quantity=len(items))
 
 
 @monuments_bp.route('/wycieczka/')
@@ -65,3 +64,21 @@ def wycieczka():
     print("3 - wycieczka_endpoint")
 
     return render_template("search.html", city=city, params=params, items=items, quantity=len(items))
+
+
+def __wyszukaj() -> Union[list[tuple], str, tuple]:
+    query_params = get_query_params()
+    city, parish, county, keyword, voivodeship, quantity = query_params.values()
+
+    if not any([city, parish, county, keyword, voivodeship]):
+        return render_template('search_main.html')
+
+    params = [": ".join(x) for x in
+              [
+                  ["miejscowość", city], ["gmina", parish], ["powiat", county],
+                  ["klucz", keyword], ["wojewodztwo", voivodeship], ["quantity", quantity]] if x[1]
+              ]
+    try:
+        items = search_monuments(**query_params)
+    except OperationalError:
+        return "Problem po stronie aplikacji. Właśnie powiadomiono administratora o zdarzeniu.", 200
